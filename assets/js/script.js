a0aa71699359f6fc81a2b01a816f4cca151444b1
Luke Cole <Lukeajcole@gmail.com>
2021-07-17 13:40:07 -0700

    New Starting Point

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,4 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://andruxnet-random-famous-quotes.p.rapidapi.com/?count=10&cat=famous",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "andruxnet-random-famous-quotes.p.rapidapi.com"
-	}
-};
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+// FLICKR API
+
+//https://type.fit/api/quotes
