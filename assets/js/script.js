f6289388ee28177484e02b5a8d470105b8d8e1da
aabazary <aabazary@gmail.com>
2021-07-17 19:55:06 -0700

    function to produce inspirational quotes with author

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
-
+var takeAHit = document.getElementById('submit-search')
+var quoteText = document.getElementById('quote-text')
 // FLICKR API
 
-//https://type.fit/api/quotes
+var quotes = 'https://type.fit/api/quotes'
+function getQuote(){
+    var quotes = 'https://type.fit/api/quotes'
+    fetch(quotes)
+    .then(res => res.json())
+    .then(data =>{
+        var i=Math.floor(Math.random() * 1642);
+        console.log(data)
+        quoteText.innerText = (data[i].text) +"--"+ (data[i].author)
+    })
+};
+takeAHit.addEventListener('click', getQuote);
