ccc1b825f513b731ef8330728f82ba49c96f3c02
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-07-18 18:27:13 -0700

    Function to include inspirational image API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
 var takeAHit = document.getElementById('submit-search')
 var quoteText = document.getElementById('quote-text')
+var image = document.getElementById('image')
+// var flickrAPIkey= "c0343f13505eba77ab00dd8ccc5fb87a";
+var imageURL= "https://api.imgflip.com/get_memes";
 // FLICKR API
+
+function getImage(){
+    fetch(imageURL)
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data);
+        var i=Math.floor(Math.random() * data.data.memes.length);
+        image.setAttribute('src', data.data.memes[i].url);
+        console.log(data.data.memes[i]);
+    })
+};
+takeAHit.addEventListener('click', getImage);
 
 var quotes = 'https://type.fit/api/quotes'
 function getQuote(){
-    var quotes = 'https://type.fit/api/quotes'
     fetch(quotes)
     .then(res => res.json())
     .then(data =>{
@@ -14,3 +28,4 @@ function getQuote(){
     })
 };
 takeAHit.addEventListener('click', getQuote);
+
