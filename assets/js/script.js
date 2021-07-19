6e5444f7ec44b441f1b02369f4e7ed7112ce7d2a
Luke Cole <Lukeajcole@gmail.com>
2021-07-18 18:39:15 -0700

    Change HTML to jQuery; Clean up JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
-var takeAHit = document.getElementById('submit-search')
-var quoteText = document.getElementById('quote-text')
-var image = document.getElementById('image')
-// var flickrAPIkey= "c0343f13505eba77ab00dd8ccc5fb87a";
+var takeAHit = $('#submit-search');
+var quoteText = $('#quote-text');
+var image = $('#image');
 var imageURL= "https://api.imgflip.com/get_memes";
-// FLICKR API
 
 function getImage(){
     fetch(imageURL)
@@ -11,21 +9,21 @@ function getImage(){
     .then(data =>{
         console.log(data);
         var i=Math.floor(Math.random() * data.data.memes.length);
-        image.setAttribute('src', data.data.memes[i].url);
+        image.attr('src', data.data.memes[i].url);
         console.log(data.data.memes[i]);
     })
 };
-takeAHit.addEventListener('click', getImage);
 
 var quotes = 'https://type.fit/api/quotes'
 function getQuote(){
+    getImage();
     fetch(quotes)
     .then(res => res.json())
     .then(data =>{
-        var i=Math.floor(Math.random() * 1642);
-        console.log(data)
-        quoteText.innerText = (data[i].text) +"--"+ (data[i].author)
+        var i=Math.floor(Math.random() * data.length);
+        console.log(data);
+        quoteText.html((data[i].text) +"--"+ (data[i].author))
     })
 };
-takeAHit.addEventListener('click', getQuote);
+takeAHit.on('click', getQuote);
 
